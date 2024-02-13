12db8a06ca22a1d80a78c6a3de5347d995a1afbb
sharpcodevelopment <159499649+sharpcodevelopment@users.noreply.github.com>
2024-02-13 16:36:37 +0300

    Update CarCredit.cs

--- CreditPayApplication/CarCredit.cs
+++ CreditPayApplication/CarCredit.cs
@@ -1,4 +1,4 @@
-﻿using System;
+a﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace CreditPayApplication
     {
         public void Pay()
         {
-            throw new NotImplementedException();
+            Console.Writeline("your loan is ready")
         }
     }
 }
